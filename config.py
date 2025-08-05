7798d07e783a9fb4b3dde4649e09422e97bb6d0b
ak4422920 <ak4422920@gmail.com>
2025-08-05 13:58:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 from os import environ 
 
 class Config:
-    API_ID = int(environ.get("API_ID", "29171167"))
-    API_HASH = environ.get("API_HASH", "7ea2149629e445936619f06a3c0dc716")
+    API_ID = int(environ.get("API_ID", ""))
+    API_HASH = environ.get("API_HASH", "")
     BOT_TOKEN = environ.get("BOT_TOKEN", "") 
-    BOT_SESSION = environ.get("BOT_SESSION", "AKautoforward_bot") 
+    BOT_SESSION = environ.get("BOT_SESSION", "ZFwdXBot") 
     DATABASE_URI = environ.get("DATABASE_URI", "")
     DATABASE_NAME = environ.get("DATABASE_NAME", "")
-    BOT_OWNER = int(environ.get("BOT_OWNER", "7251898668"))
+    BOT_OWNER = int(environ.get("BOT_OWNER", "8371607189"))
 
 class temp(object): 
     lock = {}
@@ -15,3 +15,4 @@ class temp(object):
     forwardings = 0
     BANNED_USERS = []
     IS_FRWD_CHAT = []
+
